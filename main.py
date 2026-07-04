379d4b01b63fa9ff2a7248e49645727819800ad8
Excellentmeep <Superbearjf@Gmail.com>
2026-07-04 13:47:43 -0700

    Added some comments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Get imports
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_anthropic import ChatAnthropic
@@ -6,14 +7,17 @@ from tools import search_tool, save_to_txt, wiki_tool
 
 load_dotenv()
 
+# Create general structure in the AI response
 class Answer(BaseModel):
     topic: str
     summary: str
     sources: list[str]
     tools_used: list[str]
 
+# Save Claude as a variable
 llm = ChatAnthropic(model="claude-sonnet-4-6")
 
+# Write out the system prompt
 prompt = """
 You are a video game research assistant.
 Based off the given video game, research everything you can about it, ratings, reviews, estimated time to beat, difficulty, genre, and more.
@@ -22,8 +26,10 @@ Give a brief overview of the video game itself, and list everything in a clear a
 Always save the output to a text file.
 """
 
+# Give the AI it's tools
 tools = [search_tool, save_to_txt, wiki_tool]
 
+# Using Claude, the system prompt, and the tools, create the agent
 agent = create_agent(
     model=llm,
     system_prompt=prompt,
@@ -31,7 +37,9 @@ agent = create_agent(
     response_format=Answer
 )
 
+# Create a user input for them to type in
 content = input("What would you like for me to research? ")
 
+# Invoke the agent with the user prompt, then print out the structured response
 raw_response = agent.invoke({"messages": [{"role": "user", "content": content}]})
 print(raw_response["structured_response"])
--- tools.py
+++ tools.py
@@ -1,3 +1,4 @@
+# Import everything and assign the user agent to avoid JSON error
 import wikipedia
 wikipedia.set_user_agent("Video Game Research Assistant (Jaredfrench64@Gmail.com)")
 from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
@@ -5,8 +6,10 @@ from langchain_community.utilities import WikipediaAPIWrapper
 from langchain.tools import tool
 from datetime import datetime
 
+# Assign search
 search = DuckDuckGoSearchRun()
 
+# Create the internet search tool
 @tool
 def search_tool(query: str) -> str:
     """
@@ -15,13 +18,15 @@ def search_tool(query: str) -> str:
     search.run
     return query
 
+# Create the Wikipedia search tool
 api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=1000)
 wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
 
+# Create the save to a text file tool, saves the response to a separate text file and gets a snapshot of the date and time of when it was created
 @tool
 def save_to_txt(data: str, filename: str = "Video Game Overview.txt"):
     """
-    Saves the structured teaching notes to a text file
+    Saves the structured video game overview to a text file
     """
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     formatted_text = f"--- Video Game Overview ---\nTimestamp: {timestamp}\n\n{data}\n\n"
